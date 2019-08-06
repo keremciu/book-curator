f79b7af67bc929f15770f5a8221b5b898a444482
Kerem Sevencan <kerem@ritmix.org>
2019-08-06 04:29:00 +0200

    Updates

---
+++ precache-manifest.033ae778394d745dba977af1e7090cbd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "044fae17bc59af663660c23bb0b1a6ef",
+    "url": "/book-curator/index.html"
+  },
+  {
+    "revision": "63ed6a8fa84d4c50e3d7",
+    "url": "/book-curator/static/js/2.e327e24f.chunk.js"
+  },
+  {
+    "revision": "ec33e9449b565a6be22a",
+    "url": "/book-curator/static/js/main.3bd19c5a.chunk.js"
+  },
+  {
+    "revision": "90814ef7b34dc8008920",
+    "url": "/book-curator/static/js/runtime~main.6b396dd5.js"
+  }
+]);
